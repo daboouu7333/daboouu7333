d3c81191ff521fdd765331e67603520f0fdb8b9d
dabouu <143489306+daboouu7333@users.noreply.github.com>
2025-03-29 17:47:23 +0100

    Update : Add Comment

--- C/convert_euro_dollard.c
+++ C/convert_euro_dollard.c
@@ -3,13 +3,18 @@
 
 int main()
 {
-    const float TAUX_CONVERSION = 1.08;
+    // Declaration des variables
+    const float TAUX_CONVERSION = 1.14;
     float montant_euro = 0.0, montant_dollards = 0.0;
 
+    // Demande d'un montant en euros a l'utilisateur
     printf("Donner un montant en euros : ", montant_euro);
+    // Lecture avec scanf, le & commercial devant la variable sinon ça ne fonctionne pas
     scanf("%f", &montant_euro);
 
+    // Calcul du montant
     montant_dollards = montant_euro * TAUX_CONVERSION;
+    // Affichage de la conversion
     printf("%f euro = %f dollards", montant_euro, montant_dollards);
 
     return 0;
